034c27c945d875fb9375978d38413210cf5cb924
Thanh <ltt.1112000@gmail.com>
2022-04-26 09:17:44 +0700

    Update ManageOrder.php

--- app/Http/Controllers/Admin/ManageOrder.php
+++ app/Http/Controllers/Admin/ManageOrder.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use DB;
+use Illuminate\Support\Facades\DB;
 class ManageOrder extends Controller
 {
     //show page manage order
