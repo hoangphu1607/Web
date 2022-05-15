e7af690b93487e6e922f7c0f23069c79a770369e
Thanh <ltt.1112000@gmail.com>
2022-05-15 20:27:50 +0700

    test

---
+++ resources/views/partial/modal/addMultiPhotosProduct.blade.php
@@ -0,0 +1,29 @@
+<!-- Modal -->
+<div class="modal top fade" id="multiphotosproduct" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
+    data-mdb-backdrop="true" data-mdb-keyboard="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Danh sách ảnh chi tiết của sản phẩm</h5>
+                <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="{{route('productimages')}}" data-netlify="true" enctype="multipart/form-data" id="proImages" method="POST">
+                    @csrf
+                    <div id="hidden"></div>
+                    <label for="files">Chọn hình</label>
+                    <input type="file" id="files" name="files[]" multiple><br><br>    
+                    <button type="submit" class="btn btn-primary" >Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">Close</button>
+                    
+          </div>
+          
+        </div>
+        <div class="modal-footer">
+            
+            {{-- <button type="submit" class="btn btn-primary" >Save changes</button> --}}
+        </form>
+        </div>
+    </div>
+</div>
+</div>
