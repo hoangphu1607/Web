3c371101a6e6199e8d62091ab24ac1d1a64bf6de
Thanh <ltt.1112000@gmail.com>
2022-06-07 09:19:06 +0700

    fix search
    fix lỗi box hiển thị kết quả tìm kiếm bên trang chi tiết sản phẩm

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,6 +8,7 @@
     <title>@yield('title', config('app.name', '@Master Layout'))</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     {{-- <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}"> --}}
+    <link rel="stylesheet" href="{{asset('css/home/search.css')}}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('style-libraries')
     @yield('styles')
